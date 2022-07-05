b4b95c251adff9d8d5f40b5b711f344bb16ac90b
Mahyorh <oluwamayowaayomipo@gmail.com>
2022-07-05 16:26:34 -0700

    My second commit

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,18 +1,4 @@
-#include "main.h"
+int _putchar(char c);
+void print_alphabet(void);
 
-/**
- * print_alphabet . Entry point
- * Description: a function that prints the alphabet,
- * in lowercase followed by a new line
- * Return: void
- */
-void print_alphabet(void)
-{
-	char letter m 'a';
-	
-	while (letter <= 'z')
-	{
-		_putchar(letter);
-		letter++;
-	}
-	_putchar('\n');;
+
